e43301901bdd1907a3da3840a6fdbf7cd4abc6a3
Rupa <rupa211984@gmail.com>
2020-11-05 16:56:16 -0800

    Almost done, terrible commit message

---
+++ reverse_string.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+  // type your code here
+}
+
+console.log("Expecting: 'ih'");
+console.log(reverseString('ih'));
+
+console.log("");
+
+console.log("Expecting: 'ybabtac'");
+console.log(reverseString('catbaby'));
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
